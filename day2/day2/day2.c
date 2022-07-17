26d4493915a88fd7b9dd1bcff2991549179520cb
王梓淏 <69632176+wang424@users.noreply.github.com>
2022-07-17 14:41:58 +0800

    Add files via upload

---
+++ day2/day2/day2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	a = 321;
+	b = 32;
+	float d,c;
+	c = 43.76;
+	d = 43.987;
+	
+	printf("%f", a*d);
+	printf("\n");
+	printf("%d", a * b);
+
+	return 0;
+}
